1c163685f4325a6ba30d2542d804019c5fc96c5f
ihplus-skmin <skmin@inhandplus.com>
2024-03-06 18:08:07 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	//"image/color"
+	//"time"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+
+	//"fyne.io/fyne/v2/canvas"
+
+	//"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func makeUI() (*widget.Label, *widget.Entry) {
+	out := widget.NewLabel("Hello world!")
+	in := widget.NewEntry()
+
+	in.OnChanged = func(content string) {
+		out.SetText(("Hello " + content + "!"))
+	}
+	return out, in
+}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Video Clip Uploader")
+
+	content := widget.NewButton("click me", func() {
+		log.Println("tapped")
+	})
+
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(500, 200))
+	myWindow.ShowAndRun()
+
+	tidyUp()
+}
+
+func tidyUp() {
+
+}
